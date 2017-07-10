d3d0f14119b7fc0b131027de0d7f19766347a889
bobmonkeywarts <bobmonkeywarts@gmail.com>
2017-07-10 16:14:20 -0400

    portability
    I only had like half an hour to write the original code and autotune it but now that I'm out of summer camp I could do this.

--- screams.py
+++ screams.py
@@ -1,7 +1,27 @@
 import os
+import sys
 
 data = raw_input()
-for x in data:
-	os.system("afplay " + x + ".wav")
 
- 	 
+platform = sys.platform
+
+if "linux" in platform:
+        
+        for x in data:
+
+                os.system("play " + x + ".wav")
+
+if "darwin" in platform:
+        
+        for x in data:
+
+                os.system("afplay " + x + ".wav")
+
+if "win" in platform:
+        import winsound
+        
+        for x in data:
+
+                winsound.PlaySound(x + ".wav", winsound.SND_FILENAME)
+
+print "song complete"
