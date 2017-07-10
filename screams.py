c37532d7c7aa9e22d5325f4236c8a715a335ea5b
bobmonkeywarts <bobmonkeywarts@gmail.com>
2017-07-10 16:17:40 -0400

    press space to pause for 0.2 seconds

--- screams.py
+++ screams.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import time
 data = raw_input()
 
 platform = sys.platform
@@ -8,20 +8,26 @@ platform = sys.platform
 if "linux" in platform:
         
         for x in data:
-
-                os.system("play " + x + ".wav")
+                if x == " ":
+                   time.sleep(0.2)
+                else:
+                        os.system("play " + x + ".wav")
 
 if "darwin" in platform:
         
         for x in data:
-
-                os.system("afplay " + x + ".wav")
+                if x == " ":
+                   time.sleep(0.2)
+                else:
+                        os.system("afplay " + x + ".wav")
 
 if "win" in platform:
         import winsound
         
         for x in data:
-
-                winsound.PlaySound(x + ".wav", winsound.SND_FILENAME)
+                if x == " ":
+                   time.sleep(0.2)
+                else:
+                        winsound.PlaySound(x + ".wav", winsound.SND_FILENAME)
 
 print "song complete"
